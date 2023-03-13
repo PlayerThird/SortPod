b8ee5ee082203419f692032e0b447f9629c00743
PlayerThird030 <verevka.rud@gmail.com>
2023-03-13 12:09:47 +0300

    Добавьте файлы проекта.

---
+++ SortPod/Program.cs
@@ -0,0 +1,30 @@
+﻿//сортировка подсчетом работает только с цифрами(0-9)(по умолчанию)
+using System.Threading.Tasks.Dataflow;
+
+int[] arr = { 0, 2, 3, 2, 1, 5, 9, 1, 1,7,3,8,3,7,5,2,6,8,1 };
+
+CountingSort(arr);
+Console.WriteLine(string.Join(";", arr));
+Console.ReadLine();
+void CountingSort(int[] inputArray)
+{
+    int[] counter= new int[10];//массив повторений
+	int ourNumber;
+	
+
+    for (int i = 0; i < inputArray.Length; i++)
+	{
+		//counter[inputArray[i]]
+		ourNumber = inputArray[i];
+		counter[ourNumber]++;
+	}
+    int count = 0;
+    for (int i = 0; i < counter.Length; i++)
+	{
+		for (int j = 0; j < counter[i]; j++)
+		{
+			inputArray[count] = i;
+			count++;
+		}
+	}
+}
