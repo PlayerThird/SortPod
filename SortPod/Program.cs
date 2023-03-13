f63b43cdd481ab61cf919f258af44e646811dc54
PlayerThird030 <verevka.rud@gmail.com>
2023-03-13 15:15:58 +0300

    Оптимизировал сортировку

--- SortPod/Program.cs
+++ SortPod/Program.cs
@@ -4,11 +4,18 @@ using System.Threading.Tasks.Dataflow;
 
 int[] arr = { 0, 2, 3, 2, 1, 5, 9, 1, 1, 7, 3, 8, 3, 7, 5, 2, 6, 8, 1 };
 int[] arr2 = { 0, 2, 4, 10, 20, 5, 6, 1, 2 };
+int[] arr3 = { -10, -5, -9, 10, 0, 2, 5, 1, 3,1,0,1 };
 CountingSort(arr);
 Console.WriteLine(string.Join(";", arr));
 
 arr2 = CountingSortExtended(arr2);
 Console.WriteLine(string.Join(";", arr2));
+arr3 = CountingSortExtended(arr3);
+Console.WriteLine(string.Join(";", arr3));
+
+
+
+
 Console.ReadLine();
 
 
@@ -38,22 +45,26 @@ void CountingSort(int[] inputArray)
 int[] CountingSortExtended(int[] inputArray)
 {
     int max = inputArray.Max();//ищем макс эл, чтобы задать массив равному этому числу
+    int min = inputArray.Min();
+
+    int offset = -min;//смещение, при минусовом элементе
     int[] sortedArray = new int[inputArray.Length];
-    int[] counters = new int[max + 1];//массив для подсчётов
+    int[] counters = new int[max + offset+ 1];//массив для подсчётов
 
     for (int i = 0; i < inputArray.Length; i++)
     {
-        counters[inputArray[i]]++;
+        counters[inputArray[i]+offset]++;
     }
     int count = 0;
     for (int i = 0; i < counters.Length; i++)
     {
         for (int j = 0; j < counters[i]; j++)
         {
-            sortedArray[count] = i;
+            sortedArray[count] = i- offset;
             count++;
         }
     }
 
     return sortedArray;
 }
+
